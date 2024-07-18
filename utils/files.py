f8f7eae63633f0f050b041ddd0685577301e57fb
Philippa Richter <pips.richter@gmail.com>
2024-07-18 13:04:23 -0700

    lolz i guess they are

--- utils/files.py
+++ utils/files.py
@@ -79,7 +79,7 @@ class FastaFile(File):
         seqs = [s.replace('\n', '') for s in seqs]
 
         for seq in seqs:
-            assert len(seq) < MAX_SEQ_LENGTH, f'ProteinFile.sequences: Sequence has length {len(seq)}, which exceeds the maximum allowed sequence length of {MAX_SEQ_LENGTH}.\n {seq}'
+            assert len(seq) < MAX_SEQ_LENGTH, f'ProteinFile.sequences: Sequence has length {len(seq)}, which exceeds the maximum allowed sequence length of {MAX_SEQ_LENGTH}.\n\n{seq}'
         return seqs
 
     def size(self):
