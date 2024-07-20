4c8648aed898e39875cb9bb4eb51357fd4e5d72b
Philippa Richter <pips.richter@gmail.com>
2024-07-19 20:34:15 -0700

    kms

--- app/__init__.py
+++ app/__init__.py
@@ -48,7 +48,8 @@ def get(table_name:str=None) -> Tuple[requests.Response, int, Dict[str, str]]:
         filter_(query)
 
     result = query.submit(database)
-    # return str(result[0]._fields)
+    for row in result:
+        return f'{row} {row._asdict()}'
 
     data = pd.DataFrame.from_records([row._asdict() for row in result]) #, columns=result._fields)
     return data.to_csv(), 200, {'Content-Type':'text/plain'}
