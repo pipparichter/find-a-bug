55493df8b66fe3638faab940822cd9d4a3f095e3
Philippa Richter <pips.richter@gmail.com>
2024-07-19 20:09:07 -0700

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -51,7 +51,7 @@ def get(table_name:str=None) -> Tuple[requests.Response, int, Dict[str, str]]:
     if len(result) == 0: # In case of no results.
         return 'No results', 200
     else:
-        return f'{(len(result))} {result}'
+        return str(query)
         data = pd.DataFrame.from_records([row._asdict() for row in result], columns=result[0]._fields)
         return data.to_csv(), 200, {'Content-Type':'text/plain'}
 
