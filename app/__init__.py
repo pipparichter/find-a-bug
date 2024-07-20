53bf590bdb582b30f81d2639e86ca264ed97d501
Philippa Richter <pips.richter@gmail.com>
2024-07-19 20:49:22 -0700

    d

--- app/__init__.py
+++ app/__init__.py
@@ -40,7 +40,7 @@ def get(table_name:str=None) -> Tuple[requests.Response, int, Dict[str, str]]:
     database = Database(reflect=True)
 
     from sqlalchemy import inspect 
-    return list(inspect(database.engine).get_tables())
+    return list(inspect(database.engine).get_table_names())
 
     query = Query(database, table_name, page=int(page))
 
