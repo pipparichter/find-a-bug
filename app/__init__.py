9b7adb7b04d56a61a9b208fca4530b3a1f3f99bb
Philippa Richter <pips.richter@gmail.com>
2024-08-01 22:32:03 -0700

    o

--- app/__init__.py
+++ app/__init__.py
@@ -51,7 +51,6 @@ def get(table_name:str=None) -> Tuple[requests.Response, int, Dict[str, str]]:
             filter_(query)
             
         result = query.submit(database)
-        return result
         database.close()
 
         data = pd.DataFrame.from_records([row._asdict() for row in result]) #, columns=result._fields)
