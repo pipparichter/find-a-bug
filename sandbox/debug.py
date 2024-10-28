b5ed9e21193095b048fe12d75a20eec6851755d3
Philippa Richter <pips.richter@gmail.com>
2024-10-28 14:30:39 -0700

    debug

--- sandbox/debug.py
+++ sandbox/debug.py
@@ -9,7 +9,7 @@ failing_genome_ids = np.loadtxt('failing_genome_ids.txt', dtype=str)
 DATA_DIR = '/var/lib/pgsql/data/gtdb/'
 LOG_DIR = os.path.join(os.getcwd(), '../scripts/log/')
 
-genome_ids_in_directory = os.listdir(os.path.join(DATA_DIR, 'r207', 'annotations_kegg'))
+genome_ids_in_directory = os.listdir(os.path.join(DATA_DIR, 'r207', 'proteins_aa'))
 # Remove the file extensions and prefixes...
 genome_ids_in_directory = [file_name.replace('.tab.gz', '') for file_name in genome_ids_in_directory]
 genome_ids_in_directory = [genome_id.replace('RS_', '').replace('GB_', '') for genome_id in genome_ids_in_directory]
@@ -17,7 +17,7 @@ genome_ids_in_directory = [genome_id.replace('RS_', '').replace('GB_', '') for g
 genome_ids_missing = []
 for genome_id in failing_genome_ids:
     if not (genome_id in genome_ids_in_directory):
-        print(genome_id, 'missing in annotations_kegg directory.')
+        print(genome_id, 'missing in proteins_aa directory.')
         genome_ids_missing.append(genome_id)
 
-print(len(genome_ids_missing), 'total genome IDs are not in the annotations directory.')
+print(len(genome_ids_missing), 'total genome IDs are not in the proteins_aa directory.')
