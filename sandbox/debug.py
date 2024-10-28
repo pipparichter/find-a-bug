4e0a6a295ae0a28224ab7bde227a327e2932e6e4
Philippa Richter <pips.richter@gmail.com>
2024-10-28 14:23:14 -0700

    debug

---
+++ sandbox/debug.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np 
+import os 
+
+# print('Current working directory:', os.getcwd())
+
+failing_genome_ids = np.loadtxt('failing_genome_ids.txt')
+
+DATA_DIR = '/var/lib/pgsql/data/gtdb/'
+LOG_DIR = os.path.join(os.getcwd(), '../scripts/log/')
+
+genome_ids_in_directory = os.listdir(os.path.join(DATA_DIR, 'r207', 'annotations_kegg'))
+# Remove the file extensions and prefixes...
+genome_ids_in_directory = [file_name.replace('.tab.gz', '') for file_name in genome_ids_in_directory]
+genome_ids_in_directory = [genome_ids.replace('RS_', '').replace('GB_', '') for genome_id in genome_ids_in_directory]
+
+for genome_id in failing_genome_ids:
+    if not (genome_id in genome_ids_in_directory):
+        print(genome_id, 'missing in annotations_kegg directory.')
